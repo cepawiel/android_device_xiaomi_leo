2901f158f987556e9435d2bfa834763c30917a8b
Colton Pawielski <cepawiel@mtu.edu>
2017-03-10 12:14:00 -0500

    Cleaned up cm files

--- lineage.mk
+++ lineage.mk
@@ -9,7 +9,7 @@ $(call inherit-product, device/xiaomi/leo/device_leo.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := leo
-PRODUCT_NAME := cm_leo
+PRODUCT_NAME := lineage_leo
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi Note Pro
 PRODUCT_MANUFACTURER := Xiaomi
